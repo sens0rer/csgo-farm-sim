57ce5980391e17b0b37bbc6350e9dd069f0644c9
Artem Omelchenko <56344591+sens0rer@users.noreply.github.com>
2022-11-12 20:42:32 +0200

    Extended the range of dates that can be used
    The old version was accurate all the way back to June 2021, this one is accurate all the way back to October 2019, which is a huge improvement and let's you get a lot more useful data. Not planning to go any further into the past as it's pretty much irrelevant(only interesting for historical and not practical reasons).
    Also added 'delta' parameter which lets you change the default farm cycle from 7 days to anything you want. Comes with wrongly scaled data, but lets you get more resolution and see the trends in more detail. The scale can be manually adjusted by anyone who's going to use it, but I am not bothering because it is way beyond my usecase

--- sim.py
+++ sim.py
@@ -8,46 +8,106 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Constants
-_min_date = dt.date(2020, 1, 1)
+_min_release_date = dt.date(2013, 8, 13)
+_min_rare_date = dt.date(2014, 1, 14)
 _max_date = dt.date(2030, 1, 1)
+_min_non_prime_date = dt.date(2019, 10, 26)
+
 _release_dates = {'Recoil Case': dt.date(2022, 7, 1),
-                  'Dreams & Nightmares Case': dt.date(2022, 1, 21)
+                  'Dreams & Nightmares Case': dt.date(2022, 1, 21),
+                  'Snakebite Case':  dt.date(2021, 5, 3),
+                  'Clutch Case':  dt.date(2018, 2, 15),
+                  'Fracture Case':  dt.date(2020, 8, 7),
+                  'Prisma 2 Case': dt.date(2020, 4, 1),
+                  'Danger Zone Case': dt.date(2018, 12, 6),
+                  'CS20 Case':   dt.date(2019, 10, 18),
+                  'Prisma Case':   dt.date(2019, 3, 13),
+                  'Horizon Case':   dt.date(2018, 8, 2),
+                  'Spectrum 2 Case':   dt.date(2017, 9, 14),
+                  'Operation Hydra Case':   dt.date(2017, 5, 23),
+                  'Spectrum Case':   dt.date(2017, 3, 15),
+                  'Glove Case':   dt.date(2019, 3, 13),
+                  'Gamma Case':   dt.date(2016, 6, 15),
+                  'Gamma 2 Case':   dt.date(2016, 8, 18),
+                  'Chroma 3 Case':   dt.date(2016, 4, 20),
+                  'Revolver Case':   dt.date(2015, 12, 8),
+                  'Operation Wildfire Case':   dt.date(2016, 2, 17),
+                  'Shadow Case':   dt.date(2015, 9, 17),
+                  'Falchion Case':   dt.date(2015, 5, 26),
+                  'Chroma Case':   dt.date(2015, 1, 8),
+                  'Chroma 2 Case':   dt.date(2015, 4, 15),
+                  'Operation Vanguard Weapon Case':   dt.date(2014, 11, 11),
+                  'Operation Breakout Weapon Case':   dt.date(2014, 7, 1),
+                  'Huntsman Weapon Case':   dt.date(2014, 5, 1),
+                  'Winter Offensive Weapon Case':   dt.date(2013, 12, 18),
+                  'Operation Bravo Case':   dt.date(2013, 9, 19),
+                  'CS:GO Weapon Case 2':   dt.date(2013, 11, 8),
+                  'CS:GO Weapon Case 3':   dt.date(2014, 2, 12),
+                  'CS:GO Weapon Case':   dt.date(2013, 8, 14),
+                  'Sticker Capsule': _min_release_date,
+                  'Sticker Capsule 2': _min_release_date,
+                  'Community Sticker Capsule 1': _min_release_date
                   }
-_drop_pool = {'Recoil Case':  _max_date,
-              'Snakebite Case':  _max_date,
-              'Clutch Case':  _max_date,
-              'Fracture Case':  _max_date,
-              'Dreams & Nightmares Case':  _max_date,
-              'Prisma 2 Case': dt.date(2022, 7, 1),
-              'Danger Zone Case': dt.date(2022, 1, 18),
-              'CS20 Case':   _min_date,
-              'Prisma Case':   _min_date,
-              'Horizon Case':   _min_date,
-              'Spectrum 2 Case':   _min_date,
-              'Operation Hydra Case':   _min_date,
-              'Spectrum Case':   _min_date,
-              'Glove Case':   _min_date,
-              'Gamma Case':   _min_date,
-              'Gamma 2 Case':   _min_date,
-              'Chroma 3 Case':   _min_date,
-              'Revolver Case':   _min_date,
-              'Operation Wildfire Case':   _min_date,
-              'Shadow Case':   _min_date,
-              'Falchion Case':   _min_date,
-              'Chroma Case':   _min_date,
-              'Chroma 2 Case':   _min_date,
-              'Operation Vanguard Weapon Case':   _min_date,
-              'Operation Breakout Weapon Case':   _min_date,
-              'Huntsman Weapon Case':   _min_date,
-              'Winter Offensive Weapon Case':   _min_date,
-              'Operation Bravo Case':   _min_date,
-              'CS:GO Weapon Case 2':   _min_date,
-              'CS:GO Weapon Case 3':   _min_date,
-              'CS:GO Weapon Case':   _min_date,
-              'Sticker Capsule': _min_date,
-              'Sticker Capsule 2': _min_date,
-              'Community Sticker Capsule 1': _min_date
-              }
+
+_non_prime_start_dates = {'Gamma Case': _min_non_prime_date,
+                          'Gamma 2 Case': _min_non_prime_date,
+                          'Shadow Case': _min_non_prime_date,
+                          'Chroma 2 Case': _min_non_prime_date,
+                          'Spectrum Case': _min_non_prime_date,
+                          'Revolver Case': _min_non_prime_date,
+                          'Chroma 3 Case': dt.date(2020, 4, 1),
+                          'Horizon Case': dt.date(2020, 4, 1),
+                          'Spectrum 2 Case': dt.date(2020, 4, 1),
+                          'CS20 Case': dt.date(2020, 8, 7)}
+
+_non_prime_end_dates = {'Gamma Case': dt.date(2018, 2, 15),
+                        'Gamma 2 Case': dt.date(2021, 6, 3),
+                        'Shadow Case': dt.date(2016, 8, 18),
+                        'Chroma 2 Case': dt.date(2017, 3, 15),
+                        'Spectrum Case': dt.date(2017, 3, 15),
+                        'Revolver Case': dt.date(2021, 6, 3),
+                        'Chroma 3 Case': dt.date(2021, 6, 3),
+                        'Horizon Case': dt.date(2021, 6, 3),
+                        'Spectrum 2 Case': dt.date(2021, 6, 3),
+                        'CS20 Case': dt.date(2021, 6, 3)}
+
+_non_prime_list = list(_non_prime_start_dates.keys())
+
+_rare_dates = {'Recoil Case':  _max_date,
+               'Snakebite Case':  _max_date,
+               'Clutch Case':  _max_date,
+               'Fracture Case':  _max_date,
+               'Dreams & Nightmares Case':  _max_date,
+               'Prisma 2 Case': dt.date(2022, 7, 1),
+               'Danger Zone Case': dt.date(2022, 1, 18),
+               'CS20 Case':   dt.date(2021, 6, 3),
+               'Prisma Case':   dt.date(2021, 5, 3),
+               'Horizon Case':   dt.date(2021, 6, 3),
+               'Spectrum 2 Case':   dt.date(2021, 6, 3),
+               'Operation Hydra Case':   dt.date(2017, 11, 13),
+               'Spectrum Case':   dt.date(2017, 3, 15),
+               'Glove Case':   dt.date(2018, 12, 6),
+               'Gamma Case':   dt.date(2018, 2, 15),
+               'Gamma 2 Case':   dt.date(2021, 6, 3),
+               'Chroma 3 Case':   dt.date(2021, 6, 3),
+               'Revolver Case':   dt.date(2021, 6, 3),
+               'Operation Wildfire Case':   dt.date(2017, 9, 14),
+               'Shadow Case':   dt.date(2016, 8, 18),
+               'Falchion Case':   dt.date(2015, 6, 15),
+               'Chroma Case':   dt.date(2015, 9, 9),
+               'Chroma 2 Case':   dt.date(2017, 3, 15),
+               'Operation Vanguard Weapon Case':   dt.date(2015, 3, 31),
+               'Operation Breakout Weapon Case':   dt.date(2016, 4, 20),
+               'Huntsman Weapon Case':   dt.date(2016, 4, 15),
+               'Winter Offensive Weapon Case':   _min_rare_date,
+               'Operation Bravo Case':   dt.date(2014, 2, 5),
+               'CS:GO Weapon Case 2':   _min_rare_date,
+               'CS:GO Weapon Case 3':   _min_rare_date,
+               'CS:GO Weapon Case':   _min_rare_date,
+               'Sticker Capsule': _min_rare_date,
+               'Sticker Capsule 2': _min_rare_date,
+               'Community Sticker Capsule 1': _min_rare_date
+               }
 
 
 def get_price_data(is_new: bool = True):
@@ -59,7 +119,7 @@ def get_price_data(is_new: bool = True):
     """
     price_data = {}
     dateformat = "%Y-%m-%d %H:%M:%S"
-    case_list = list(_drop_pool.keys())
+    case_list = list(_rare_dates.keys())
     l = len(case_list)
     for i, case in enumerate(case_list):
         if is_new:
@@ -126,8 +186,8 @@ class Structure():
                  acc_num=100,
                  start_date=dt.date(2021, 6, 9),
                  start_bal=0):
-        if start_date < dt.date(2021, 5, 4):
-            exception = f"""Only use dates after 04.05.2021, using earlier
+        if start_date < dt.date(2019, 10, 27):
+            exception = f"""Only use dates after 27.10.2019, using earlier
 dates results in inaccurate data.
 
 Date entered: {start_date}"""
@@ -144,23 +204,29 @@ class DropManager():
     """
 
     def __init__(self, drop_pool, release_dates, date):
-        self._drop_pool = drop_pool
+        self._rare_dates = drop_pool
         self._release_dates = release_dates
         self._release_date_list = list(release_dates.values())
         self.date = date
         self._active_pool = []
         self._rare_pool = []
         # Initialize drop pools
-        for case in self._drop_pool.keys():
-            removal_date = self._drop_pool.get(case)
-            release_date = self._release_dates.get(case, _min_date)
-            if date < removal_date and date >= release_date:
+        for case in self._rare_dates.keys():
+            rare_date = self._rare_dates.get(case)
+            release_date = self._release_dates.get(
+                case, _min_release_date)
+            if case in _non_prime_list:
+                ignore_start = _non_prime_start_dates[case]
+                ignore_end = _non_prime_end_dates[case]
+                if date >= ignore_start and date < ignore_end:
+                    continue
+            if date < rare_date and date >= release_date:
                 self._active_pool.append(case)
                 continue
             if date >= release_date:
                 self._rare_pool.append(case)
 
-    def _update_drop_pool(self, date):
+    def _update_rare_dates(self, date):
         # Check if it's time to update the drop pools
         run_update = False
         if self._release_date_list != []:
@@ -177,10 +243,16 @@ class DropManager():
         if run_update:
             self._active_pool = []
             self._rare_pool = []
-            for case in self._drop_pool.keys():
-                removal_date = self._drop_pool.get(case)
-                release_date = self._release_dates.get(case, _min_date)
-                if date < removal_date and date >= release_date:
+            for case in self._rare_dates.keys():
+                rare_date = self._rare_dates.get(case)
+                release_date = self._release_dates.get(
+                    case, _min_release_date)
+                if case in _non_prime_list:
+                    ignore_start = _non_prime_start_dates[case]
+                    ignore_end = _non_prime_end_dates[case]
+                    if date >= ignore_start and date < ignore_end:
+                        continue
+                if date < rare_date and date >= release_date:
                     self._active_pool.append(case)
                     continue
                 if date >= release_date:
@@ -205,13 +277,14 @@ class Farm(Structure, DropManager):
                  start_bal=0,
                  drift: int = 0,
                  steam_to_irl=0.7,
-                 cases_to_sell=[]):
+                 cases_to_sell=[],
+                 delta: int = 7):
         Structure.__init__(self,
                            acc_num,
                            start_date,
                            start_bal)
         DropManager.__init__(self,
-                             _drop_pool,
+                             _rare_dates,
                              _release_dates,
                              start_date)
         self.price_data = price_data
@@ -225,6 +298,9 @@ class Farm(Structure, DropManager):
         # Cases that are sold instantly(to enable you to stash others)
         self.insta_sell = cases_to_sell
         self.stash = []
+        # Time between cycles of farming
+        # Better left same as default value for simulation accuracy
+        self.delta = delta
 
     def _price_for_date(self, price_data, date, hour=0):
         datetime = dt.datetime.combine(date, dt.time(hour, 0, 0))
@@ -237,12 +313,13 @@ class Farm(Structure, DropManager):
         dated_price_data = {}
         for case in price_data.keys():
             if case in self._active_pool or case in self._rare_pool:
+                # Костиль
                 try:
                     date_index = price_data[case]["Date"].index(
                         datetime)
                 except ValueError:
                     date_index = price_data[case]["Date"].index(datetime
-                                                                + dt.timedelta(hours=7, minutes=0))
+                                                                + dt.timedelta(hours=8, minutes=0))
                 dated_price_data[case] = price_data[case]["Price(USD)"][date_index]
 
         return dated_price_data
@@ -250,7 +327,7 @@ class Farm(Structure, DropManager):
     def run_once(self):
         # Get drops and update balance
         drops = []
-        self._update_drop_pool(self.date)
+        self._update_rare_dates(self.date)
         for i in range(self.acc_num):
             drops.append(self._drop_event())
 
@@ -263,7 +340,7 @@ class Farm(Structure, DropManager):
                 self.stash.append(case)
 
         # Update date
-        self.date += dt.timedelta(days=7)
+        self.date += dt.timedelta(days=self.delta)
         if self.hour + self.drift > 23:
             self.date += dt.timedelta(days=1)
             self.hour = (self.hour+self.drift) % 24
@@ -274,7 +351,7 @@ class Farm(Structure, DropManager):
         stats = {"Date": [],
                  "Balance": []
                  }
-        while self.date <= date:
+        while self.date <= date - dt.timedelta(days=self.delta):
             stats["Date"].append(self.date)
             self.run_once()
             stats["Balance"].append(self.balance)
@@ -369,17 +446,19 @@ def plot(stats, mode=0):
 if __name__ == "__main__":
     price_data = get_price_data(False)
     farm1 = Farm(price_data,
-                 acc_num=5000,
-                 start_date=dt.date(2022, 7, 1),
+                 acc_num=10000,
+                 start_date=dt.date(2019, 10, 27),
                  start_bal=0,
-                 drift=2,
+                 drift=0,
                  steam_to_irl=0.6,
-                 cases_to_sell=["Recoil Case", "Dreams & Nightmares Case"])
-    stats = get_deep_stats(
-        farm1.run_til_date(dt.date(2022, 10, 28)),
-        acc_pr=11.5,
-        acc_num=farm1.acc_num)
-    modes = [1, 3, 5, 6]
+                 cases_to_sell=[],
+                 delta=7)
+    stats = farm1.run_til_date(dt.date(2022, 11, 8))
+    stats = get_deep_stats(stats,
+                           acc_pr=11.5,
+                           acc_num=farm1.acc_num,
+                           drift=farm1.drift)
+    modes = [0, 1, 2, 3, 4, 5, 6]
     for mode in modes:
         plot(stats, mode)
     print(farm1.stash_value())
